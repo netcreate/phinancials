32f0ee34a9cc332ce13ade160ceec831fc633171
Philip Kocanda <philip@kocanda.nl>
2016-06-29 21:41:39 +0200

    Add basic ListView

--- js/Overview.ios.js
+++ js/Overview.ios.js
@@ -1,36 +1,101 @@
 /**
+ * https://facebook.github.io/react-native/docs/listview.html
+ *
  * @flow
  */
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
   Text,
-  TextInput,
+  StyleSheet,
+  TouchableHighlight,
+  RecyclerViewBackedScrollView,
   View,
-  TabBarIOS,
-  NavigatorIOS
+  ListView
 } from 'react-native';
 
 class Overview extends Component {
+  constructor() {
+    super();
+
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+  }
+
+  _renderRow(rowData: string, sectionID: number, rowID: number, highlightRow: (sectionID: number, rowID: number) => void) {
+    var rowHash = Math.abs(hashCode(rowData));
+    // var imgSource = THUMB_URLS[rowHash % THUMB_URLS.length];
+    return (
+      <TouchableHighlight onPress={() => {
+          this._pressRow(rowID);
+          highlightRow(sectionID, rowID);
+        }}>
+        <View>
+          <View style={styles.row}>
+            {/*<Image style={styles.thumb} source={imgSource} />*/}
+            <Text style={styles.text}>
+              {rowData + ' - ' + LOREM_IPSUM.substr(0, rowHash % 301 + 10)}
+            </Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
+  _genRows(pressData: {[key: number]: boolean}): Array<string> {
+    var dataBlob = [];
+    for (var ii = 0; ii < 100; ii++) {
+      var pressedText = pressData[ii] ? ' (pressed)' : '';
+      dataBlob.push('Row ' + ii + pressedText);
+    }
+    return dataBlob;
+  }
+
+  _pressRow(rowID: number) {
+    this._pressData[rowID] = !this._pressData[rowID];
+    this.setState({dataSource: this.state.dataSource.cloneWithRows(
+      this._genRows(this._pressData)
+    )});
+  }
+
+  _renderSeperator(sectionID: number, rowID: number, adjacentRowHighlighted: bool) {
+    return (
+      <View
+        key={`${sectionID}-${rowID}`}
+        style={{
+          height: adjacentRowHighlighted ? 4 : 1,
+          backgroundColor: adjacentRowHighlighted ? '#3B5998' : '#CCCCCC',
+        }}
+      />
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Phinancials!
-        </Text>
-        <Text style={styles.instructions}>
-          The easy-as-hell financials application.
-        </Text>
-        <Text style={styles.instructions}>
-          Tap "New form" for a new expenses form.
-          This is the overview.
-        </Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderRow}
+          renderScrollComponent={props => <RecyclerViewBackedScrollView {...props} />}
+          renderSeparator={this._renderSeperator}
+        />
       </View>
     );
   }
 }
+
+const LOREM_IPSUM = 'Lorem ipsum dolor sit amet, ius ad pertinax oportere accommodare, an vix civibus corrumpit referrentur. Te nam case ludus inciderint, te mea facilisi adipiscing. Sea id integre luptatum. In tota sale consequuntur nec. Erat ocurreret mei ei. Eu paulo sapientem vulputate est, vel an accusam intellegam interesset. Nam eu stet pericula reprimique, ea vim illud modus, putant invidunt reprehendunt ne qui.';
+
+const hashCode = function(str) {
+  var hash = 15;
+  for (var ii = str.length - 1; ii >= 0; ii--) {
+    hash = ((hash << 5) - hash) + str.charCodeAt(ii);
+  }
+  return hash;
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -39,23 +104,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: '#F6F6F6',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  thumb: {
+    width: 64,
+    height: 64,
   },
-  textInput: {
-    height: 40,
-    width:200,
-    borderColor: 'grey',
-    backgroundColor: 'white',
-    borderWidth: 1
-  }
+  text: {
+    flex: 1,
+  },
 });
 
 module.exports = Overview;
