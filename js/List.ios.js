c1e40600d647636adfd19225a92087d3cd1bb139
Philip Kocanda <philip@kocanda.nl>
2016-08-02 22:46:03 +0200

    Extract list from overview and introduce navigator

---
+++ js/List.ios.js
@@ -0,0 +1,127 @@
+/**
+ * https://facebook.github.io/react-native/docs/listview.html
+ *
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Text,
+  StyleSheet,
+  TouchableHighlight,
+  RecyclerViewBackedScrollView,
+  View,
+  ListView
+} from 'react-native';
+
+class List extends Component {
+  constructor() {
+    super();
+    this._renderRow = this._renderRow.bind(this);
+
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        'row 1',
+        'row 2',
+        'row 3',
+        'row 4',
+        'row 5',
+        'row 6',
+        'row 7',
+        'row 8',
+        'row 9',
+        'row 10',
+        'row 11',
+        'row 12',
+        'row 13',
+        'row 14',
+      ]),
+    };
+  }
+
+  _renderRow(rowData: string, sectionID: number, rowID: number, highlightRow: (sectionID: number, rowID: number) => void) {
+    var rowHash = Math.abs(hashCode(rowData));
+    // var imgSource = THUMB_URLS[rowHash % THUMB_URLS.length];
+
+    return (
+      <TouchableHighlight onPress={() => {
+          this._pressRow(rowID);
+          highlightRow(sectionID, rowID);
+        }}>
+        <View>
+          <View style={styles.row}>
+            {/*<Image style={styles.thumb} source={imgSource} />*/}
+            <Text style={styles.text}>
+              {rowData + ' - ' + LOREM_IPSUM.substr(0, rowHash % 301 + 10)}
+            </Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
+  _pressRow(rowID: number) {
+    alert('lol');
+  }
+
+  _renderSeperator(sectionID: number, rowID: number, adjacentRowHighlighted: bool) {
+    return (
+      <View
+        key={`${sectionID}-${rowID}`}
+        style={{
+          height: adjacentRowHighlighted ? 4 : 1,
+          backgroundColor: adjacentRowHighlighted ? '#3B5998' : '#CCCCCC',
+        }}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ListView
+          style={styles.listView}
+          dataSource={this.state.dataSource}
+          renderRow={this._renderRow}
+          renderScrollComponent={props => <RecyclerViewBackedScrollView {...props} />}
+          renderSeparator={this._renderSeperator}
+          automaticallyAdjustContentInsets={false}
+        />
+      </View>
+    );
+  }
+}
+
+const LOREM_IPSUM = 'Lorem ipsum dolor sit amet, ius ad pertinax oportere accommodare, an vix civibus corrumpit referrentur. Te nam case ludus inciderint, te mea facilisi adipiscing. Sea id integre luptatum. In tota sale consequuntur nec. Erat ocurreret mei ei. Eu paulo sapientem vulputate est, vel an accusam intellegam interesset. Nam eu stet pericula reprimique, ea vim illud modus, putant invidunt reprehendunt ne qui.';
+
+const hashCode = function(str) {
+  var hash = 15;
+  for (var ii = str.length - 1; ii >= 0; ii--) {
+    hash = ((hash << 5) - hash) + str.charCodeAt(ii);
+  }
+  return hash;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    paddingBottom: 0,
+  },
+  listView: {
+    marginTop: 0,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#F6F6F6',
+  },
+  text: {
+    flex: 1,
+  },
+});
+
+module.exports = List;
